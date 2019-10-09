b9f12c921d4f536013b9d592e02a07d135e812a9
Andrew Esteban <andrewesteban1999@gmail.com>
2019-10-09 13:41:30 +1100

    added move booleans

--- src/main/java/andrew/Board.java
+++ src/main/java/andrew/Board.java
@@ -6,5 +6,28 @@ public class Board {
 
     // Normal tetris board is 10*20
     // Use a 10*24 board to allow for hidden rotations
-    
+
+    // If we use the ID method for storing locations in a matrix
+    // then we need two extra columns and an extra row for -1
+
+    private int[][] matrix;
+
+    public Board() {
+        this.matrix = new int[24][12];
+
+        // Set bottom row as -1
+        for (int i = 0; i < this.matrix[this.matrix.length - 1].length; i++) {
+            this.matrix[this.matrix.length - 1][i] = -1;
+        }
+
+        // Set left and right columns as -1
+        for (int i = 0; i < this.matrix.length; i++) {
+            this.matrix[i][0] = -1;
+            this.matrix[i][this.matrix[i].length - 1] = -1;
+        }
+    }
+
+    public int[][] getMatrix() {
+        return this.matrix;
+    }
 }
--- src/main/java/andrew/Piece.java
+++ src/main/java/andrew/Piece.java
@@ -1,19 +1,67 @@
 package andrew;
 
+import java.util.ArrayList;
+
 public class Piece {
 
     private Board board;
+    private int id;
+    private ArrayList<int[]> coordinates;
 
+    public Piece(ArrayList<int[]> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public ArrayList<int[]> getCoordinates() {
+        return this.coordinates;
+    }
+
+    public boolean rotate() {
+        return false;
+    }
 
     public boolean canMoveDown() {
 
+        boolean canMove = true;
+
+        int[][] matrix = this.board.getMatrix();
+
+        for (int[] co : this.coordinates) {
+            if (matrix[co[1] + 1][co[0]] != 0 && matrix[co[1] + 1][co[0]] != this.id) {
+                canMove = false;
+            }
+        }
+
+        return canMove;
     }
 
     public boolean canMoveLeft() {
+        
+        boolean canMove = true;
 
+        int[][] matrix = this.board.getMatrix();
+
+        for (int[] co : this.coordinates) {
+            if (matrix[co[1]][co[0] - 1] != 0 && matrix[co[1]][co[0] - 1] != this.id) {
+                canMove = false;
+            }
+        }
+
+        return canMove;
     }
 
     public boolean canMoveRight() {
+        
+        boolean canMove = true;
 
+        int[][] matrix = this.board.getMatrix();
+
+        for (int[] co : this.coordinates) {
+            if (matrix[co[1]][co[0] + 1] != 0 && matrix[co[1]][co[0] + 1] != this.id) {
+                canMove = false;
+            }
+        }
+
+        return canMove;
     }
 }
